f864f015c949f4f3bafad4e4271e45c5f9cf8141
RadiatingVibe <dhruvselopalrocks@gmail.com>
2025-06-23 14:15:59 +0530

    pushing

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <uwebsockets/App.h>
+#include<nlohmann/json.hpp>
+#include <stdio.h>
+#include "iostream"
+#include"math.h"
+#include<queue>
+#include<unordered_map>
+
+int main()
+{
+    // nlohmann::json * json = new nlohmann::json(); 
+    // auto app = uWS::App();
+    // app.post("/main", [](uWS::HttpResponse<false> *res, uWS::HttpRequest *req)
+    //          {
+    //             req->get
+    //          });
+    // app.listen(9001, [](auto *token)
+    //            {
+    //     if(token) std::cout << "Server listeneing on 9001"; });
+    // app.run();
+    std::unordered_map<int,std::pair<int,int>> myMap;
+    std::cout << "Hello World";
+    
+}
