873a0c48175c50b283321489509e7bf3455bedec
RadiatingVibe <dhruvselopalrocks@gmail.com>
2025-06-24 16:39:42 +0530

    working on post
    practice with std::unorderd_map and post

--- main.cpp
+++ main.cpp
@@ -3,30 +3,38 @@
 #include <stdio.h>
 #include "iostream"
 #include<string>
+#include<unordered_map>
+
+using nlohmann::json ;
 
 int main()
 {
-    nlohmann::json * json = new nlohmann::json(); 
-    auto app = uWS::App();
-    app.post("/main", [](uWS::HttpResponse<false> *res, uWS::HttpRequest *req)
-             {
-                res->onAborted([]() {
-                    std::cout << "[ABORTED] Client disconnected before body finished.\n";
-                });
-                res->onData([res,body = std::string{}](std::string_view chunk,bool isLast) mutable{
-                    body.append(chunk);
-                    if(isLast){
-                        nlohmann::json j = nlohmann::json::parse(body);
-                        for(auto &[key,value] : j.items()){
-                            std::cout << key << " " << value << std::endl;
-                        }
-                        res->end("ended");
-                    }
-                });
-             });
-    app.listen(9001, [](auto *token)
-               {
-        if(token) std::cout << "Server listeneing on 9001"; });
-    app.run();
-    
+    std::unordered_map<std::string,std::string>  * db = new std::unordered_map<std::string,std::string>();
+    uWS::App * app = new uWS::App();
+
+    app->post("/main",[db](uWS::HttpResponse<false> * res, uWS::HttpRequest * req){
+        json * j = new json();
+        res->onAborted([res](){
+            std::cout << "Aborted";
+        });
+        res->onData([res,body=std::string{},j,db](std::string_view chunk , bool isLast) mutable{
+            body.append(chunk);
+            if(isLast){
+                *j = json::parse(body);
+                std::unordered_map<std::string,std::string> tempMap;
+                tempMap = *j;
+                db->insert(tempMap.begin(),tempMap.end());
+                for(auto &[key,value] : *db){
+                std::cout << std::endl <<  key << " " << value << std::endl;
+                }
+                res->end("Ended");
+            }
+        });
+    });
+    app->listen(9001,[](auto * token){
+        if(token) std::cout << "Server listening on 9001";
+    });
+    app->run();
+
+    return 0;
 }
